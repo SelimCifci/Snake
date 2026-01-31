982b94516e8721d94b7e6a6251777bcf5875828f
SelimCifci <cifciselimhasan@gmail.com>
2026-01-31 11:50:36 +0100

    Fixed bugs

--- main.py
+++ main.py
@@ -1,118 +1,122 @@
 import pygame
 import random
 
-background_color = (20,46,16)
-(width, height) = (480, 480)
-
-clock = pygame.time.Clock()
-
-screen = pygame.display.set_mode((width, height))
-pygame.display.set_caption('Snake')
-
-block_size_pixels = 30
-
-direction = [1,0]
+class Vector2:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
 
 class Snake:
-    def __init__(self, start_blocks, block_size):
+    def __init__(self, start_blocks, block_size, screen):
         self.block_size = block_size
+        self.screen = screen
+        self.margin = block_size / 10
+        self.size = self.block_size - self.margin*2
 
         self.blocks = []
-        self.head_rect = None
         self.grow = False
+        self.direction = Vector2(1,0)
+        self.old_direction = Vector2(1,0)
+        self.position = Vector2(1,0)
 
-        for nmb in range(start_blocks):
-            self.blocks.append((0, nmb*self.block_size))
+        for i in range(start_blocks):
+            self.blocks.append(Vector2(0, i))
 
     def move(self):
-        new_head = (direction[0]*self.block_size + self.blocks[0][0], direction[1]*self.block_size + self.blocks[0][1])
+        self.position = Vector2(self.blocks[0].x + self.direction.x, self.blocks[0].y + self.direction.y)
 
-        self.blocks.insert(0, new_head)
+        self.blocks.insert(0, self.position)
         if not self.grow:
             self.blocks.pop(-1)
         else:
             self.grow = False
 
-        self.head_rect = pygame.Rect(new_head[0], new_head[1], 30, 30)
-
     def draw(self):
-        for snake_part in self.blocks:
-            rect = pygame.Rect(snake_part[0]+3, snake_part[1]+3, 24, 24)
-            pygame.draw.rect(screen, (85, 107, 48), rect, 0)
+        for block in self.blocks:
+            rect = pygame.Rect(block.x*self.block_size+self.margin, block.y*self.block_size+self.margin, self.size, self.size)
+            pygame.draw.rect(self.screen, (85, 107, 48), rect, 0)
 
 class Apple:
-    def __init__(self, block_size):
+    def __init__(self, block_size, screen):
         self.block_size = block_size
+        self.screen = screen
+        self.margin = block_size / 10
+        self.size = self.block_size - self.margin*2
 
-        self.rect = None
+        self.position = Vector2(0,0)
+
+        self.spawn()
 
     def spawn(self):
-        margin = self.block_size/10
-
-        position = round(random.random()*self.block_size/2)*self.block_size
-        self.rect = pygame.Rect(position+margin, position+margin, self.block_size-margin*2, self.block_size-margin*2)
+        self.position.x = random.randint(0, self.screen.get_width() // self.block_size - 1)
+        self.position.y = random.randint(0, self.screen.get_height() // self.block_size - 1)
 
     def draw(self):
-        pygame.draw.rect(screen, (85, 107, 48), self.rect)
-
-snake = Snake(3, block_size_pixels)
-apple = Apple(block_size_pixels)
-
-def logic():
-    if snake.head_rect.colliderect(apple.rect):
-        snake.grow = True
-        apple.spawn()
-
-    for block in snake.blocks:
-        rect = pygame.Rect(block[0]+1, block[1]+1, block[0]+block_size_pixels-1, block[1]+block_size_pixels-1)
-        if snake.head_rect.colliderect(rect):
-            print("Collided")
-
-def draw():
-    snake.draw()
-    apple.draw()
+        rect = pygame.Rect(self.position.x*self.block_size+self.margin, self.position.y*self.block_size+self.margin, self.size, self.size)
+        pygame.draw.rect(self.screen, (135, 157, 98), rect)
 
 def main():
-    apple.spawn()
+    background_color = (20,46,16)
+    width, height = 480, 480
 
-    old_direction = direction.copy()
+    clock = pygame.time.Clock()
 
-    snake.move()
+    screen = pygame.display.set_mode((width, height))
+    pygame.display.set_caption('Snake')
+
+    block_pixels = 30
+
+    snake = Snake(3, block_pixels, screen)
+    apple = Apple(block_pixels, screen)
 
     running = True
     while running:
         screen.fill(background_color)
 
-        clock.tick(2)
+        clock.tick(4)
 
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+            match event.type:
+                case pygame.QUIT:
+                    running = False
+                case pygame.KEYDOWN:
+                    match event.key:
+                        case pygame.K_LEFT:
+                            if snake.old_direction.x != 1:
+                                snake.direction.x = -1
+                                snake.direction.y = 0
+                        case pygame.K_RIGHT:
+                            if snake.old_direction.x != -1:
+                                snake.direction.x = 1
+                                snake.direction.y = 0
+                        case pygame.K_DOWN:
+                            if snake.old_direction.y != -1:
+                                snake.direction.x = 0
+                                snake.direction.y = 1
+                        case pygame.K_UP:
+                            if snake.old_direction.y != 1:
+                                snake.direction.x = 0
+                                snake.direction.y = -1
+
+        snake.move()
+
+        if snake.position.__eq__(apple.position):
+            snake.grow = True
+            apple.spawn()
+
+        for block in snake.blocks[1:-1]:
+            if snake.blocks[0].__eq__(block):
                 running = False
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
-                    if old_direction[0] != 1:
-                        direction[0] = -1
-                        direction[1] = 0
-                if event.key == pygame.K_RIGHT:
-                    if old_direction[0] != -1:
-                        direction[0] = 1
-                        direction[1] = 0
-                if event.key == pygame.K_DOWN:
-                    if old_direction[1] != -1:
-                        direction[0] = 0
-                        direction[1] = 1
-                if event.key == pygame.K_UP:
-                    if old_direction[1] != 1:
-                        direction[0] = 0
-                        direction[1] = -1
 
-        logic()
-        draw()
+        snake.draw()
+        apple.draw()
 
-        old_direction = direction.copy()
+        snake.old_direction = Vector2(snake.direction.x, snake.direction.y)
 
-        pygame.display.set_icon(screen)
-        pygame.display.update()
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
